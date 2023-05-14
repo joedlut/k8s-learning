5edfcaa99d04b4020830c8fb0f7f03849d2688bf
root <2532771263@qq.com>
2023-05-14 13:28:36 +0800

    add files

---
+++ harbor/readme.txt
@@ -0,0 +1,4 @@
+/etc/docker/daemon.json添加 并且重启docker 才可以推送
+
+ "insecure-registries": ["192.168.88.138:80"]
+
---
+++ harbor/tag.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker tag fortune:1.1 192.168.88.138:80/library/fortune:1.1
+docker tag fortune:1.1 192.168.88.138:80/joedlut/fortune:1.1
