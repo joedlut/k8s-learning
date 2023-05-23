4bfd62f4eca9ff454ef255cb464a66b9aa44fcbb
root <2532771263@qq.com>
2023-05-23 19:28:13 +0800

    add files

---
+++ chapter8/1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ls -l  /var/run/secrets/kubernetes.io/serviceaccount
+
+
+kubectl create clusterrolebinding permissive-binding  --clusterrole=cluster-admin --group=system:serviceaccounts
+
+TOKEN=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`
+
+curl -H   "Authorization: Bearer $TOKEN"  --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt   https://10.96.0.1:443
+
+
+NS =`cat /var/run/secrets/kubernetes.io/serviceaccount/token/namespace`
+curl -H   "Authorization: Bearer $TOKEN"  --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt   https://10.96.0.1:443/api/v1/namespaces/$NS/pods
+
+
+
