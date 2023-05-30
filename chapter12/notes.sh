c215ed0bd1ad2aa7a100e91a9283662db3647adb
root <2532771263@qq.com>
2023-05-30 09:04:20 +0800

    add files

--- chapter12/notes.sh
+++ chapter12/notes.sh
@@ -6,3 +6,16 @@ kubectl create serviceaccount fo
 
 # 启用RBAC
 kubectl delete clusterrolebinding permissive-binding o
+
+
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=2532771263@qq.com
+
+
+#通过命令行创建角色
+kubectl create role service-reader --verb=get --verb=list --resource=services -n bar
+
+#绑定role到 serviceaccount上
+kubectl create rolebinding test --role=service-reader --serviceaccount=foo:default -n foo
+
+#命令修改rolebinding 
+ k edit rolebinding test -n foo
