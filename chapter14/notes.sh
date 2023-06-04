418d74fc0f01d6e824c31845d383af687a24aaac
root <2532771263@qq.com>
2023-06-04 23:57:20 +0800

    20230604

---
+++ chapter14/notes.sh
@@ -0,0 +1,12 @@
+root@joedlut-virtual-machine:/home/joedlut/k8s-learning/chapter14# k describe quota
+Name:            cpu-and-mem
+Namespace:       default
+Resource         Used   Hard
+--------         ----   ----
+limits.cpu       1200m  600m
+limits.memory    120Mi  500Mi
+requests.cpu     1100m  400m
+requests.memory  30Mi   200Mi
+
+
+
