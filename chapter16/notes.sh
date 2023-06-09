960498b5afcea8fa1d4f2aeefd12807f3ece67e7
root <2532771263@qq.com>
2023-06-09 23:49:25 +0800

    add files

--- chapter16/notes.sh
+++ chapter16/notes.sh
@@ -3,3 +3,8 @@
 k taint node k8s-worker-node1 node-type=production:NoSchedule
 
 k taint node k8s-worker-node2 node-type=non-production:NoSchedule --overwrite
+
+#去掉污点
+k taint node  k8s-worker-node2 node-type=non-production:NoSchedule-
+
+kubectl run backend -l app=backend --image busybox -- sleep 999999
